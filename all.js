22278f351e988c9ab9d10be95b677a988e564b20
larry <larrylinr5@gmail.com>
2021-11-05 13:22:53 +0800

    動態調整下拉選單

--- all.js
+++ all.js
@@ -1,12 +1,98 @@
-//程式啟動進入點
-init();
+/** 下拉式選單全域變數 */
+let selectUnitValue='0'
+/** 下拉式選單全域變數 */
+let selectCityValue='0'
 
+
+//程式啟動進入點
+init('View');
+
+
+
+/** 全域變數初始化 */
+function resetAreaVariable (){
+    selectUnitValue='0';
+    selectCityValue='0';
+}
+
+/** 選擇渲染哪一個畫面的方法 */
+function choosePage (e){
+    console.log('target',e.srcElement.className)
+    const pageClass = e.srcElement.className
+
+    if(pageClass==='headerRightView'){
+        init('View')
+    }
+    else if(pageClass==='headerRightStay'){
+        init('Stay')
+    }
+    else{
+        init('Traffic')
+    }
+}
+
+axios.get('https://gist.motc.gov.tw/gist_api/V3/Map/Basic/City').then(
+    response=>{
+        console.log('response>>>',response.data)
+    }
+)
 
 
 
 /** 程式啟動進入點 */
-function init() {
-    ShowHotCityArea(true);
+function init(pageString) {
+    const reflashSelectUnit =(selectUnitList)=>{
+        document.getElementById('selectUnit').innerHTML=getSelectUnitString(selectUnitList)
+    }
+
+    //將全域變數還原初始化
+    resetAreaVariable()
+
+    //製作 台灣景點 頁面
+    if(pageString==='View'){
+        const selectUnitList = [
+            {
+                name:'類別',
+                value:'0'
+            },
+            {
+                name:'景點',
+                value:'View'
+            },
+            {
+                name:'活動',
+                value:'Active'
+            },
+        ]
+
+        BuildSelectCity()
+        reflashSelectUnit(selectUnitList)
+        ShowHotCityArea(true);
+    }
+    //製作 美食住宿 頁面
+    else if(pageString==='Stay'){
+        const selectUnitList = [
+            {
+                name:'類別',
+                value:'0'
+            },
+            {
+                name:'美食',
+                value:'Food'
+            },
+            {
+                name:'住宿',
+                value:'Stay'
+            },
+        ]
+
+        BuildSelectCity()
+        reflashSelectUnit(selectUnitList)
+    }
+    //製作 景點交通 頁面
+    else{
+
+    }
 }
 
 //#region 熱門城市相關方法
@@ -176,4 +262,66 @@ function ChooseHotCity(bool) {
     }
 }
 //#endregion
+
+//#region 下拉式選單相關方法
+
+/** 縣市下拉選單組成方法 */
+function BuildSelectCity (){
+    const CityObj =[
+        {CityName: '不分縣市', City: '0'},
+        {CityName: '臺北市', City: 'Taipei'},
+        {CityName: '新北市', City: 'NewTaipei'},
+        {CityName: '桃園市', City: 'Taoyuan'},
+        {CityName: '臺中市', City: 'Taichung'},
+        {CityName: '臺南市', City: 'Tainan'},
+        {CityName: '高雄市', City: 'Kaohsiung'},
+        {CityName: '基隆市', City: 'Keelung'},
+        {CityName: '新竹市', City: 'Hsinchu'},
+        {CityName: '新竹縣', City: 'HsinchuCounty'},
+        {CityName: '苗栗縣', City: 'MiaoliCounty'},
+        {CityName: '彰化縣', City: 'ChanghuaCounty'},
+        {CityName: '南投縣', City: 'NantouCounty'},
+        {CityName: '雲林縣', City: 'YunlinCounty'},
+        {CityName: '嘉義縣', City: 'ChiayiCounty'},
+        {CityName: '嘉義市', City: 'Chiayi'},
+        {CityName: '屏東縣', City: 'PingtungCounty'},
+        {CityName: '宜蘭縣', City: 'YilanCounty'},
+        {CityName: '花蓮縣', City: 'HualienCounty'},
+        {CityName: '臺東縣', City: 'TaitungCounty'},
+        {CityName: '金門縣', City: 'KinmenCounty'},
+        {CityName: '澎湖縣', City: 'PenghuCounty'},
+        {CityName: '連江縣', City: 'LienchiangCounty'}
+    ]
+    let ResaultString=''
+
+    CityObj.forEach(item=>{
+        ResaultString+=`
+        <option value=${item.City}>${item.CityName}</option>
+        `
+    })
+
+    document.getElementById('selectCity').innerHTML=ResaultString
+}
+
+/** 組下拉式選單的字串 */
+function getSelectUnitString(selectUnitList){
+    let ResaultString=''
+    selectUnitList.forEach(item=>{
+        ResaultString+=`<option value=${item.value}>${item.name}</option>`
+    })
+    return ResaultString
+}
+
+/** 將下拉選單的值存放至全域變數 */
+function selectFun(selectItem){
+    const e = document.getElementById(selectItem);
+    const strUser = e.options[e.selectedIndex].value;
+    if(selectItem==='selectUnit') selectUnitValue = strUser
+    if(selectItem==='selectCity') selectCityValue = strUser
+}
+
+//#endregion
+
+
+
 
